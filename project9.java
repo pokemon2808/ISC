e74b151179f9051ead436791b6598107d59c676d
muh.arif28 <115476882+pokemon2808@users.noreply.github.com>
2023-10-15 21:59:10 +0800

    Update project9.java

--- project9.java
+++ project9.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 public class project9{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        // Meminta jumlah karyawan dari pengguna
         System.out.print("Masukkan jumlah karyawan: ");
         int jumlahKaryawan = input.nextInt();
 
-        // Meminta jumlah bulan dari pengguna
         System.out.print("Masukkan jumlah bulan: ");
         int jumlahBulan = input.nextInt();
 
-        // Inisialisasi variabel untuk menyimpan total gaji
         double totalGaji = 0;
 
         for (int bulan = 1; bulan <= jumlahBulan; bulan++) {
@@ -23,16 +19,12 @@ public class project9{
                 System.out.print("Masukkan gaji karyawan ke-" + i + " untuk bulan ini: ");
                 double gaji = input.nextDouble();
 
-                // Menambahkan gaji ke totalGaji
                 totalGaji += gaji;
             }
         }
 
-        // Menampilkan jumlah karyawan dan total gaji
         System.out.println("Jumlah karyawan: " + jumlahKaryawan);
         System.out.println("Total gaji selama " + jumlahBulan + " bulan: " + totalGaji);
-
-        // Menutup Scanner
         input.close();
     }
 
