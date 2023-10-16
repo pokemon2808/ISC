d0adc0cc71388ab6162f49b354e6c8c797d51767
pokemon2808 <dedeaqzhar12@gmail.com>
2023-10-16 21:38:07 +0800

    codingan array sederhana

---
+++ project10.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class project10 {
+    public static void main(String[] args) {       
+         int[] angka = {10, 20, 30, 40, 50}; 
+         System.out.println("Elemen ke-1: " + Arrays.toString(angka)); 
+        }
+    }
+    
+
