ce0d2cc6a0fd4a5008fda4dfef936b30746bada8
pokemon2808 <dedeaqzhar12@gmail.com>
2023-10-19 23:24:49 +0800

    menampilkan elemen array menggunakan loop for-each

---
+++ project12.java
@@ -0,0 +1,10 @@
+public class project12 {
+    public static void main(String[] args) {
+         System.out.println("tampilkan elemen array menggunakan for - each");
+        int[] nomor = {8,1,7,2,10,6,4,3,9,5};
+        for(int item : nomor) {
+       
+        System.out.println("nomor:"+ item);
+    }
+}
+}
