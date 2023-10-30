10f84b13ac673a3fa8bd47539d7720b5d373f373
muh.arif28 <115476882+pokemon2808@users.noreply.github.com>
2023-10-30 23:53:39 +0800

    buat rumus lingkarang menggunakan class dan object

---
+++ project23.java
@@ -0,0 +1,12 @@
+public class project23 {
+        public static void main(String[] args) {
+            double luaslingkarang = bangundatarlingkaran.hitungluas(49);
+            System.out.println("luas lingkaran ="+ luaslingkarang);
+
+            double kelilinglingkarang = bangundatarlingkaran.hitungkeliling(49);
+            System.out.println("kelilinglingkarang = "+ kelilinglingkarang);
+            }
+             
+        }
+    
+
