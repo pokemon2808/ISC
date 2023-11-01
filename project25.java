7b98dc6908593080fdeaabcce8b417d72db1901a
muh.arif28 <115476882+pokemon2808@users.noreply.github.com>
2023-11-01 23:21:51 +0800

    membuat sebuah method this

---
+++ project25.java
@@ -0,0 +1,11 @@
+public class project25 {
+    public void printMessage() {
+        System.out.println(" halo saya menggunakan this untuk codingan saya ");
+    }
+
+    public static void main(String[] args) {
+        project25 myObject = new project25();
+        myObject.printMessage();
+    }
+}
+
