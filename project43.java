8034bed2067003f51c9d935543fee89126554f13
muh.arif28 <115476882+pokemon2808@users.noreply.github.com>
2023-11-19 22:41:14 +0800

    membuat bilangan ganjil

---
+++ project43.java
@@ -0,0 +1,8 @@
+
+public class project43 {
+    public static void main(String[] args) {
+        for(int i= 1;i<=99; i +=2 ){
+            System.out.println("nilai bilangan ganjil :"+i);
+        }
+    }
+}
